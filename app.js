96c2d47b4fa787ba1877ee66b3f013bd72d9e2b3
Gilina McBride <gilinamcbride@gmail.com>
2022-06-07 14:42:04 -0400

    add functions to generate html page and refactor

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+
+fs.writeFile("index.html", generatePage(name, github), (err) => {
+  if (err) throw new Error(err);
+  console.log("Portfolio complete! Check out index.html to see the output!");
+});
 
 //arrow function with only one paramter doesn't need ()
-const printProfileData = (profileDataArr) => {
-  profileDataArr.forEach((profileItem) => console.log(profileItem));
-};
-printProfileData(profileDataArgs);
+// for each array method example:
+// const printProfileData = (profileDataArr) => {
+//   profileDataArr.forEach((profileItem) => console.log(profileItem));
+// };
+// printProfileData(profileDataArgs);
