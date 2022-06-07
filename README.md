6984876db306e69920d2a5ae6377d4b14f8ae4dc
Gilina McBride <gilinamcbride@gmail.com>
2022-06-07 18:11:03 -0400

    refactored with promises instead of callbacks

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
-# portfolio-generator
+## Portfolio Generator
+
+# User
+
+Create a portfolio of your work using the command line.
+Simply enter your information based on the questions and then node.js will created an html file for your specific portfolio
+
+# Languages
+
+Node.js
+Javascript
+HTML
+CSS
+
+# Application
+
+To run this application:
+
+- Clone the code
+- Install npm inquirer
+- Launch terminal/bash in your directory
+- Call "node app" in the command line
--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const inquirer = require("inquirer");
-const fs = require("fs");
+const { writeFile, copyFile } = require("./utils/generate-site.js");
 const generatePage = require("./src/page-template.js");
 
 const promptUser = () => {
@@ -142,18 +142,18 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    const pageHTML = generatePage(portfolioData);
-    fs.writeFile("./index.html", pageHTML, (err) => {
-      if (err) throw new Error(err);
-      console.log(
-        "Page created! Check out index.html in this directory to see it!"
-      );
-    });
+    return generatePage(portfolioData);
+  })
+  .then((pageHTML) => {
+    return writeFile(pageHTML);
+  })
+  .then((writeFileResponse) => {
+    console.log(writeFileResponse);
+    return copyFile();
+  })
+  .then((copyFileResponse) => {
+    console.log(copyFileResponse);
+  })
+  .catch((err) => {
+    console.log(err);
   });
-
-//arrow function with only one paramter doesn't need ()
-// for each array method example:
-// const printProfileData = (profileDataArr) => {
-//   profileDataArr.forEach((profileItem) => console.log(profileItem));
-// };
-// printProfileData(profileDataArgs);
