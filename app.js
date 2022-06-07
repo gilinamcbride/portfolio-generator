efd4ddb26244f776b435b385f8827151acb984c3
Gilina McBride <gilinamcbride@gmail.com>
2022-06-07 17:22:06 -0400

    generating data to page

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
 const inquirer = require("inquirer");
-// const fs = require("fs");
-// const generatePage = require("./src/page-template.js");
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-// const pageHTML = generatePage(name, github);
-
-// fs.writeFile("index.html", pageHTML, (err) => {
-//   if (err) throw new Error(err);
-//   console.log("Portfolio complete! Check out index.html to see the output!");
-// });
 const promptUser = () => {
   return inquirer.prompt([
     {
@@ -148,8 +142,15 @@ const promptProject = (portfolioData) => {
 promptUser()
   .then(promptProject)
   .then((portfolioData) => {
-    console.log(portfolioData);
+    const pageHTML = generatePage(portfolioData);
+    fs.writeFile("./index.html", pageHTML, (err) => {
+      if (err) throw new Error(err);
+      console.log(
+        "Page created! Check out index.html in this directory to see it!"
+      );
+    });
   });
+
 //arrow function with only one paramter doesn't need ()
 // for each array method example:
 // const printProfileData = (profileDataArr) => {
