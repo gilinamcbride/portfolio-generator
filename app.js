010acb78018846482e7f33eab41f88769e13a023
Gilina McBride <gilinamcbride@gmail.com>
2022-06-07 15:47:10 -0400

    set up inquirer

--- app.js
+++ app.js
@@ -1,12 +1,23 @@
-const fs = require("fs");
-const generatePage = require("./src/page-template.js");
-const profileDataArgs = process.argv.slice(2);
-const [name, github] = profileDataArgs;
+const inquirer = require("inquirer");
+// const fs = require("fs");
+// const generatePage = require("./src/page-template.js");
 
-fs.writeFile("index.html", generatePage(name, github), (err) => {
-  if (err) throw new Error(err);
-  console.log("Portfolio complete! Check out index.html to see the output!");
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile("index.html", pageHTML, (err) => {
+//   if (err) throw new Error(err);
+//   console.log("Portfolio complete! Check out index.html to see the output!");
+// });
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      name: "input",
+      message: "What is your name?",
+    },
+  ])
+  .then((answers) => console.log(answers));
 
 //arrow function with only one paramter doesn't need ()
 // for each array method example:
